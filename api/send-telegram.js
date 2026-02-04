eddb38a719175bbcbb4941bee9dda4538c6b504f
MAXIOS <user@maxios.co.il>
2026-02-04 19:09:07 +0200

    Add method logging to debug order issue

--- api/send-telegram.js
+++ api/send-telegram.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method not allowed', received_method: req.method });
   }
 
   const { type, data } = req.body || {};
